b067faac76c8f1a04a55fc266b3856239c40cb44
owner <owner@DESKTOP-S2T1N6L>
2021-05-26 10:36:53 +0900

    커밋

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -3,8 +3,8 @@ package kr.or.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// main 자바프로그램의 진입 메서드(자동실행) TODO Auto-generated method stub
-		System.out.println("헬로 자바!");
+		// 자바 앱은 메인 메서드 진입점이 필요, main 자바프로그램의 진입 메서드(자동실행)
+		System.out.println("헬로 자바 !!!");
 	}
 
 }
