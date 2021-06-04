bad0d1d42673dd3b981d3829a9767c05c09ae2de
seoje <seoje@test.test>
2021-06-04 12:49:51 +0900

    0604 금 점심시간 커밋

--- DATABASES/sql_0604.sql
+++ DATABASES/sql_0604.sql
@@ -1,3 +1,5 @@
+--8장 함수(count, upper, lower, to_char, round..) 그룹함수
+SELECT ename, round(sal) FROM emp;
 --DDL문(create; alter; drop;), DCL문(commit; rollback;)
 --DML문(Data Manufacture Language) insert, update, delete
 --insert문: 테이블에 새로운 레코드(row)를 추가
@@ -20,3 +22,14 @@ SELECT * FROM dept02 order by deptno;
 --delete는 레코드 1줄을 지우는 명령
 DELETE FROM dept02; -- 이렇게 사용하면 모든 레코드가 삭제됨. 사용주의필요
 DELETE FROM dept02 WHERE deptno >= 0; --모두 삭제 where 반드시 포함
+
+-- Drop table 테이블명은 테이블 자체를 물리적으로 없애는 명령
+DROP TABLE dept02; -- 드롭테이블은 커밋없이 바로 적용됨.
+CREATE TABLE emp01 AS SELECT * FROM emp; -- 테이블 복제명령
+SELECT * FROM emp01;
+--Update 테이블명 set 필드명 = '바꿀값' where empno='특정값'
+UPDATE emp01 SET ename = '홍길동' where empno = 7839;
+ROLLBACK; --롤백은 마지막 커밋 바로 전까지 되돌린다
+UPDATE emp01 SET sal = sal*1.1; -- 모든 직원의 연봉을 10%인상
+UPDATE emp01 SET hiredate = sysdate;
+--머지 merge는 표준쿼리(ANSI쿼리)가 아니기 때문에 생략
--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -47,9 +47,12 @@ public class DataSourceTest {
 		//stmt객체가 없으면, 개발자가 SQL인젝션 방지코딩을 넣어야함
 		//Insert쿼리 문장을 만듬(아래)
 		//예전 방식으로 더미데이터(샘플)를 100개 입력
-		for(int cnt=0;cnt<100;cnt++) { //error deptno 자리수가 2로 고정되어서 100은 입력시 에러
-			stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')");
-		}
+		
+		/*
+		 * for(int cnt=0;cnt<100;cnt++) { //error deptno 자리수가 2로 고정되어서 100은 입력시 에러
+		 * stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')"); }
+		 */
+		
 		//인서트, 업데이트, 삭제시 sql디벨로퍼에서는 커밋이 필수지만, 외부 java클래스는 자동커밋이 된다.
 		//테이블에 입력되어있는 레코드를 select 쿼리 stmt 문장으로 가져옴(아래)
 		ResultSet rs = stmt.executeQuery("Select * from dept02 order by deptno");//옛날방식
@@ -58,6 +61,8 @@ public class DataSourceTest {
 			// rs라는 객체의 레코드가 없을때까지 무한반복
 			logger.debug(rs.getString("deptno")+" "+rs.getString("dname")+" "+rs.getString("loc"));
 		}
+		stmt = null; // 메모리 반환
+		rs = null; // 메모리 반환
 		connection = null; //메모리 초기화
 	}
 	@Test
