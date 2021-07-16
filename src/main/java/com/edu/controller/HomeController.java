19e4a399b84fb08784a838ae69c02b9b4e16195b
owner <owner@DESKTOP-S2T1N6L>
2021-07-16 14:23:29 +0900

    0716 목 5교시 프로젝트마무리 커밋

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -277,7 +277,8 @@ public class HomeController {
 	@RequestMapping(value="/join_form", method=RequestMethod.GET)
 	public String join_form() throws Exception {
 		
-		return "home/join"; // .jsp생략
+		//return "home/join"; // .jsp생략 , 타일즈 적용 전
+		return "join.tiles"; // tiles폴더 안에 join.jsp 뷰 호출
 	}
 	//마이페이지에서 회원탈퇴 POST방식 처리
 	@RequestMapping(value="member/mypage_leave", method=RequestMethod.POST)
